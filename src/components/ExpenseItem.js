faa6fea489a9691850f451cb4d9c9e90ffc40c2f
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-07 17:37:02 +0200

    Added little bit more of html into component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>First Expense React component :)</h2>;
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>amount</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
