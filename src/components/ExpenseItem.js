87f3dd5ac916b571d07a22d6aa3b50aa9afd8b03
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-10 09:51:56 +0200

    Dynamic data into component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 5, 10);
+  const expenseDescription = "A Spending";
+  const expenseAmount = 268.12;
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>amount</div>
+    <div className="expense-item">
+      <div>{expenseDate.getUTCDate}</div>
+      <div className="expense-item__description">
+        <h2>{expenseDescription}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
