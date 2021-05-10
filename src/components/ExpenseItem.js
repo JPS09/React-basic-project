c19ce7dbb688a3757d0521e26c55f56b75e41c34
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-10 10:25:43 +0200

    Added data in app component alongside props in expense item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 5, 10);
-  const expenseDescription = "A Spending";
-  const expenseAmount = 268.12;
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.getUTCDate}</div>
+      <div>{props.date.toUTCString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseDescription}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
