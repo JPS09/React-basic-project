9338232305e5885a49cd233d7578bfaf5f1c7be0
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-12 10:15:04 +0200

    Using useState for updating title

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "./Card.js";
 
 function ExpenseItem(props) {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
+    setTitle("Fun!");
     console.log("Much click");
-    title = "Changing the title";
   };
   return (
     <Card className="expense-item">
