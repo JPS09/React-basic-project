b676ba97e430ba567d58839ef3fa4890754a06ba
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-12 10:08:34 +0200

    Changed function name to reflect style guide and added variable to change title on click

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,16 +3,19 @@ import ExpenseDate from "./ExpenseDate.js";
 import Card from "./Card.js";
 
 function ExpenseItem(props) {
-  const clickTest = () =>{
-    console.log('oh boy')
-  }
+  let title = props.title;
+
+  const clickHandler = () => {
+    console.log("Much click");
+    title = "Changing the title";
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
-        <button onClick={clickTest}>This a test</button>
+        <button onClick={clickHandler}>This a test</button>
       </div>
     </Card>
   );
