18c17942b6c43a5fff0a7da469aa3c6cb0b0171d
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-05-17 10:07:19 +0200

    Displaying list based on data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,26 +15,14 @@ function Expenses(props) {
         onSelectedYear={selectedYearHandler}
         selected={filteredYear}
       ></ExpenseFilter>
-      <ExpenseItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
-      />
-      <ExpenseItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
-      />
-      <ExpenseItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
-      />
-      <ExpenseItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
-      />
+      {props.data.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
